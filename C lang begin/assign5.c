63b523718e20481d39bedc74706e3103585965ca
ankitha2577 <ankitmkavu@gmail.com>
2026-01-28 23:36:22 +0530

    Add files via upload
    area of rectangle

---
+++ C lang begin/assign5.c
@@ -0,0 +1,14 @@
+
+
+#include<stdio.h>
+void main()
+{
+float length,breadth,area;
+printf("enter length");
+scanf("%f",&length);
+printf("enter breadth");
+scanf("%f",&breadth);
+area = length*breadth;
+printf("The area of rectangle is:%f",area);
+}
+
