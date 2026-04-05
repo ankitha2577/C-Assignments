dc34771bc4cb1b261984d30bdf0ccb844f6111d3
ANKITHA <ankitmkavu@gmail.com>
2026-04-05 18:50:07 +0530

    Add files via upload

---
+++ C lang begin/assign8.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+//  odd or even
+int main() {
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    if (num % 2 == 0) {
+        printf("NUMBER IS EVEN\n");
+    }
+    if (num % 2 != 0) {
+        printf("NUMBER IS ODD\n");
+    }
+    return 0;
+}
