aafdf00483bf61767d15161c5f542a32913e76ce
ANKITHA <ankitmkavu@gmail.com>
2026-04-05 18:55:01 +0530

    Add files via upload

---
+++ C lang begin/assign15.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int x, y;
+    printf("Enter value of x: ");
+    scanf("%d", &x);
+
+    if (y = (x > 0) ? 1 : 0)
+   
+
+    printf("y = %d\n", y);
+    return 0;
+}
